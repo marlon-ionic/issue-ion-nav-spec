a02e5eb0ced5efa2d9133ccf1241ee8533278433
Marlon Harrison <marlon@ionic.io>
2023-09-01 09:40:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'issue-ion-nav-spec',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
